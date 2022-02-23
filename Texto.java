854416cab90d9fbaf3cf0254e203f0b916afa3b5
GYOVANNE MARTINS SABARÁ <22102360@CFP127ALUNO.LOCAL>
2022-02-23 10:56:38 -0300

    Atualização de código

---
+++ Texto.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Texto{
+    public static void main(String args[]){
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("\n Olá gentil senhorio ou senhorita, qual és a ta idade");
+        int idade = ler.nextInt();
+        int an = 2022 - idade;
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t Olá ser maravilhoso que nasceu em " + an);
+    }
+}
